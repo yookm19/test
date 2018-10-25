9de66a1a24c6db1f18a86d72f577956d08a67ddb
yookm19 <yookm19@gmail.com>
2018-10-26 01:34:16 +0900

    完了

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 // POSTメソッドで渡される値を取得、表示
-$inputString = file_get_contents('php://input');
+//$inputString = file_get_contents('php://input');
 error_log($inputString);
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
