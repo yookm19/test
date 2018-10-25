115ebb199e45756f2c8304d6be756ee3c96e2002
yookm19 <yookm19@gmail.com>
2018-10-25 20:34:11 +0900

    完了

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo "Hell, World!"
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+// POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input');
+error_log($inputString)
+
 ?>
