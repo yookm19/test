93d9b84df3e361cab255a6a48de24e2c722abbb3
yookm19 <yookm19@gmail.com>
2018-10-26 22:10:47 +0900

    完了

--- index.php
+++ index.php
@@ -20,7 +20,24 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'),
 // 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
 	//テキストを返信
-	$bot->replyText($event->getReplyToken(), 'TextMessage');
+	// $bot->replyText($event->getReplyToken(), 'TextMessage');
+
+	// テキストを返信し次のイベントの処理へ
+	replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
 }
+
+// テキストを返信。引数はLINEBot、返信先、テキスト
+function replyTextMessage($bot, $replyToken, $text) {
+	// 返信を行ないレスポンスを取得
+	// TextMessageBuilderの引数はテキスト
+	$response = $bot->replymessage($replyToken, new \LIne\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+	// レスポンスが異常な場合
+	if(!$response->isSucceeded()) {
+		//エラー内容を出力
+		error_log('Failed!'. $response->getHTTPStatus . ' '. $response->getRawBody());
+	}
+}
+
 
 ?>
