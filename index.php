97d8e5c71efb62b6ef6fd787c868e9dd44a98a65
yookm19 <yookm19@gmail.com>
2018-10-27 17:15:03 +0900

    完了

--- index.php
+++ index.php
@@ -9,22 +9,21 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 // LINE Messaging APIがリクエストに付与した署名を取得
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader
-							::LINE_SIGNATURE];
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader ::LINE_SIGNATURE];
 							
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
 // 不正であれば例外の内容を出力
-//try {
+try {
   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-// } catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-//   error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-// } catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-//   error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
-// } catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-//   error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
-// } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-//   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
-// }
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) { 
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
 
 // $midFile = __DIR__ . "/files/mids";
 
@@ -43,9 +42,9 @@ $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader
 
 
 // // 配列に格納された各イベントをループで処理
-// foreach ($events as $event) {
-// 	//テキストを返信
-// 	$bot->replyText($event->getReplyToken(), 'TextMessage');
+foreach ($events as $event) {
+	//テキストを返信
+	$bot->replyText($event->getReplyToken(), 'TextMessage');
 
 //   // テキストメッセージでなければ処理をスキップ
 //   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
@@ -61,7 +60,7 @@ $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader
     
 //   }
 
-//}
+}
 
 replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
 
