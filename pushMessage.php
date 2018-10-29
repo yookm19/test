241ee7567910e72cc0821fcfef9d0d107919f022
yookm19 <yookm19@gmail.com>
2018-10-29 18:11:44 +0900

    完了

--- pushMessage.php
+++ pushMessage.php
@@ -10,39 +10,47 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 
 $userId = 'Udeadbeefdeadbeefdeadbeefdeadbeef';
 // error_log($userId);
+
+//To 
+$to = 'Udeadbeefdeadbeefdeadbeefdeadbeef'; 
+//Channel_Access_Token 
+$Channel_Access_Token = getenv('CHANNEL_ACCESS_TOKEN');
+
+$mes = 'テストメッセージです';
+
+$response_format = [ 
+"type" => "text", 
+"text" => $mes 
+];
+
+$post_data = [ 
+"to" => $to, 
+"messages" => [$response_format] 
+];
+
+//post 
+$channel = curl_init("https://api.line.me/v2/bot/message/push"); 
+curl_setopt($channel, CURLOPT_POST, true); 
+curl_setopt($channel, CURLOPT_CUSTOMREQUEST, 'POST'); 
+curl_setopt($channel, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($channel, CURLOPT_POSTFIELDS, json_encode($post_data)); 
+curl_setopt($channel, CURLOPT_HTTPHEADER, array( 
+'Content-Type: application/json; charset=UTF-8', 
+'Authorization: Bearer ' . $Channel_Access_Token 
+));
+
+$result = curl_exec($channel); 
+// var_dump($result); 
+curl_close($channel); 
+
+
+
+//---
 //pushTextMessage($bot, $userId, "Hello World!");
-$response = $bot->pushMessage('Udeadbeefdeadbeefdeadbeefdeadbeef', new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("Hello World!"));
-if(!$response->isSucceeded()) {
-	//エラー内容を出力
-	error_log('Failed! '. $response->getHTTPStatus . ' '. $response->getRawBody());
-}
-
-
-// テキストを返信。引数はLINEBot、返信先、テキスト
-function replyTextMessage($bot, $replyToken, $text) {
-	// 返信を行いレスポンスを取得
-	// TextMessageBuilderの引数はテキスト
-	$response = $bot->replymessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
-
-	// レスポンスが異常な場合
-	if(!$response->isSucceeded()) {
-		//エラー内容を出力
-		error_log('Failed! '. $response->getHTTPStatus . ' '. $response->getRawBody());
-	}
-}
-
-// テキストをプッシュ。引数はLINEBot、返信先、テキスト
-function pushTextMessage($bot, $userId, $text) {
-	// メッセージのプッシュを行いレスポンスを取得
-	// TextMessageBuilderの引数はテキスト
-	$response = $bot->pushMessage($userId, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
-
-	// レスポンスが異常な場合
-	if(!$response->isSucceeded()) {
-		//エラー内容を出力
-		error_log('Failed! '. $response->getHTTPStatus . ' '. $response->getRawBody());
-	}
-}
-
+// $response = $bot->pushMessage('Udeadbeefdeadbeefdeadbeefdeadbeef', new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("Hello World!"));
+// if(!$response->isSucceeded()) {
+// 	//エラー内容を出力
+// 	error_log('Failed! '. $response->getHTTPStatus . ' '. $response->getRawBody());
+// }
 
 ?>
